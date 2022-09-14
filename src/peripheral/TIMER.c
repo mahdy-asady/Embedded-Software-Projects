fd03cb88d56e67e6d478704f6cabf508c9b77e04
Mahdy Asady <mahdy.asady@gmail.com>
2022-09-14 20:08:50 +0430

    Accessing 16bit Registers don't need temporary variable in c

--- src/peripheral/TIMER.c
+++ src/peripheral/TIMER.c
@@ -19,9 +19,7 @@ void EnableTimer1(void (*CallBack)(void), unsigned int IntervalInMSec) {
     fnCallBack = CallBack;
 
     //create 1miliseconds friction of time comprator value
-    unsigned int scale = F_CPU / 1000 / 256;
-    OCR1AH = scale >> 8;
-    OCR1AL = scale;
+    OCR1A = F_CPU / 1000 / 256;
 
     TCCR1B = 0 << CS10 | 0 << CS11 | 1 << CS12; //use 256 prescaler
     TIMSK |= 1 << OCIE1A; //enable compare interrupt
