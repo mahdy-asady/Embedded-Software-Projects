66dee0c2f65b3377555e62c604f4467dae4147e9
Mahdy Asady <mahdy.asady@gmail.com>
2022-09-28 15:13:30 +0330

    New Definition TIM_ENABLED created to toggle timer interrupt

--- src/mcu/stm32/stm32f1xx_it.c
+++ src/mcu/stm32/stm32f1xx_it.c
@@ -204,11 +204,11 @@ void SysTick_Handler(void)
 void TIM2_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM2_IRQn 0 */
-
+  #ifdef TIM_ENABLED
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
-
+  #endif
   /* USER CODE END TIM2_IRQn 1 */
 }
 
